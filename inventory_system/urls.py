8fb8e906065023df78d2e88444c8c88682889042
SSmikey <Seangnon09@gmail.com>
2025-09-06 13:07:22 +0700

    add api prod

--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -15,8 +15,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.http import JsonResponse
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Inventory API",
+        default_version='v1',
+        description="API สำหรับระบบสินค้าคงคลัง",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+def home(request):
+    return JsonResponse({"message": "Welcome to Inventory API!"})
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
+    path('api/', include('products.urls')),
+
+    # JWT Auth
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Swagger Docs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
