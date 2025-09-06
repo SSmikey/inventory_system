70e4c5ea04ae412828b939b7d9ed72c72935c9a8
SSmikey <Seangnon09@gmail.com>
2025-09-06 12:51:44 +0700

    mod prod

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models heres.
+import uuid
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    sku = models.CharField(max_length=100, unique=True)
+    category = models.CharField(max_length=100, blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
