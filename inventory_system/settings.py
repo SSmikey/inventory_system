06f44944d995471dd75e1fc1ee65bd7812c1232f
SSmikey <Seangnon09@gmail.com>
2025-09-17 01:37:55 +0700

    add supabase

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # จาก Supabase
+        'USER': 'postgres',  # จาก Supabase
+        'PASSWORD': '.zhxPB+Nspz2@9S',  # จาก Supabase
+        'HOST': 'db.eylpsfkbrahmhlonvvcg.supabase.co',  # จาก Supabase
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
