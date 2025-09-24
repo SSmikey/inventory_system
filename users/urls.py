2e0f92d22e1a211208b9d04adc098376dadcf7d5
SSmikey <Seangnon09@gmail.com>
2025-09-24 17:02:53 +0700

    conf token user

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import RegisterView, UserListView, UserMeView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/users/', UserListView.as_view(), name='user-list'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/me/', UserMeView.as_view(), name='user-me'),
 ]
