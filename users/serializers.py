1a51dbe9163b9ef66f2505cd8605976ce24f5b91
SSmikey <Seangnon09@gmail.com>
2025-09-24 18:34:17 +0700

    update regis serializer

--- users/serializers.py
+++ users/serializers.py
@@ -23,10 +23,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        user = User(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            role=validated_data.get('role', 'staff'),
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            role=validated_data.get('role', 'staff')  # 👈 รองรับ role
         )
         user.set_password(validated_data['password'])
         user.save()
--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ from .models import User
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
-    serializer_class = UserSerializer
+    serializer_class = RegisterSerializer
 
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
