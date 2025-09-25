d5d6e43606c4315d7707505b8535bd3e7fe54ac4
SSmikey <Seangnon09@gmail.com>
2025-09-25 14:30:28 +0700

    set data sql

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -82,10 +82,16 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('postgresql://inven_db_user:qn5SkxzDyAxfKCpzOpMkWqjdVM4WyPdS@dpg-d3adob0dl3ps73enjjbg-a/inven_db'))
-}
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.environ.get('postgresql://inven_db_user:qn5SkxzDyAxfKCpzOpMkWqjdVM4WyPdS@dpg-d3adob0dl3ps73enjjbg-a/inven_db'))
+# }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
